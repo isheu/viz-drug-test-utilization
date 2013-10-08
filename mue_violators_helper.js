40e529fb8b070c322e987352669ff3e56db05af9
unknown <isheu@isheu7.win.sphereinstitute.org>
2013-10-07 18:31:15 -0700

    Added ComboBox

--- mue_violators_helper.js
+++ mue_violators_helper.js
@@ -1,4 +1,3 @@
-/* Annotation tab to right of Line Chart*/
 function reel_label(hcpcs_div, code) {
    function gen_reel_label() {
       d3.select("div#" + hcpcs_div).selectAll("div#bubble_" + code)
@@ -633,3 +632,89 @@ function stackedbar(hcpcs, stacked_div_id, plot_id, codename) {
    }
    return gen_stackedbar;
 }
+
+// ComboBox Implementation
+$(function() {
+   $.widget("ui.combobox", {
+      _create: function() {
+         var self = this;
+         var select = this.element.hide(), 
+            selected = select.children(":selected"), 
+            value = selected.val() ? selected.text() : "";
+         var input = $("<input />").insertAfter(select)
+            .addClass("input_field")
+            .val("CHOOSE A PROVIDER")
+            .tooltip({
+               tooltipClass: "ui-state-highlight"
+            })
+            .autocomplete({
+               delay: 0,
+               minLength: 0,
+               source: function(request, response) {
+                  var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i");
+                  response( select.children("option").map(function() {
+                     var text = $(this).text();
+                     if (this.value && (!request.term || matcher.test(text) ))
+                        return {
+                           label: text.replace(
+                              new RegExp("(?![^&;]+;)(?!<[^<>]*)(" + $.ui.autocomplete.escapeRegex(request.term) + ")(?![^<>]*>)(?![^&;]+;)", "gi"),
+                              "<strong>$1</strong>"),
+                           value: text, 
+                           option: this
+                           };
+                  }) );
+               },
+               select: function(event, ui) {
+                  ui.item.option.selected = true;
+                  self._trigger( "selected", event, {
+                     item: ui.item.option
+                  });
+               },
+               change: function(event, ui) {
+                  if ( !ui.item ) {
+                     var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( $(this).val() ) + "$", "i"),
+                     valid = false;
+                     select.children( "option" ).each(function() {
+                        if ( this.value.match( matcher )) {
+                           this.selected = valid = true;
+                           return false;
+                        }
+                     });
+                     if ( !valid ) {
+                        $(this).val("");
+                        select.val("");
+                        return false;
+                     }
+                  }
+               }
+            })
+            .addClass("li_sel_value ui-widget ui-widget-content ui-corner-left ui-state-default");
+         input.data( "uiAutocomplete" )._renderItem = function( ul, item ) {
+            return $( "<li></li>" )
+              .data( "item.autocomplete", item )
+              .append( "<a>" + item.label + "</a>" )
+              .addClass( "li_values" )
+              .appendTo( ul );
+         };
+         $( "<button> </button>" )
+         .attr( "tabIndex", -1 )
+         .attr( "title", "Show All Providers" )
+         .tooltip()
+         .insertAfter( input )
+         .button({
+            icons: { primary: "ui-icon-triangle-1-s" },
+            text: false
+         })
+         .removeClass( "ui-corner-all" )
+         .addClass( "ui-corner-right ui-button-icon combobox_button" )
+         .click(function() {
+            if (input.autocomplete("widget").is(":visible")) {
+              input.autocomplete("close");
+              return;
+            }
+            input.autocomplete("search", "");
+            input.focus();
+         });
+      }
+   });
+});
